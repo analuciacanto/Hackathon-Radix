ecb351e9504f168bee2499d2ed7e2f47f3e496ea
Andrei Benincasa <andrei.benincasa@gmail.com>
2019-07-06 21:00:33 -0300

    Criando o projeto e as três primeira telas.

---
+++ StatusEnergy/app/src/main/java/com/abs/statusenergy/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.abs.statusenergy
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        cvReligar.setOnClickListener {
+            startActivity(Intent(this, ReligarActivity::class.java))
+        }
+
+        cvNova.setOnClickListener {
+            startActivity(Intent(this, NovaActivity::class.java))
+        }
+
+        cvTrocar.setOnClickListener {
+            startActivity(Intent(this, TrocarActivity::class.java))
+        }
+    }
+}
---
+++ StatusEnergy/app/src/main/java/com/abs/statusenergy/ReligarActivity.kt
@@ -0,0 +1,12 @@
+package com.abs.statusenergy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReligarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_religar)
+    }
+}
